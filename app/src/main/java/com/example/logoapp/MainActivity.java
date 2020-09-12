b62d2ad31ff856cfb4bc2a70d3972aa954f69a19
Gonzalo Lemos <gonzalo_lemos87@hotmail.com>
2020-09-12 19:26:45 -0300

    Material Design Colors

---
+++ app/src/main/java/com/example/logoapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.logoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.*;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+}
